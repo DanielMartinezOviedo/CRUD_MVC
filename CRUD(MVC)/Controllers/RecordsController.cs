0b75ee3b94409b7d3d2b745739eb7332cec3ee73
DanielMartinezOviedo <hectordanmtzo@gmail.com>
2023-03-23 18:49:42 -0600

    Adding searching function

--- CRUD(MVC)/Controllers/RecordsController.cs
+++ CRUD(MVC)/Controllers/RecordsController.cs
@@ -9,116 +9,126 @@ using CRUD_MVC_.Models.ViewModels;
 
 namespace CRUD_MVC_.Controllers
 {
-    public class RecordsController : Controller
-    {
-        // GET: Records
-        public ActionResult Index()
-        {
-            List<ListRecordsViewModels> list;
-            using (mydbEntities db = new mydbEntities())
-            {
-                 list = (from d in db.Records
-                            select new ListRecordsViewModels
-                            {
-                                Id = d.Personid,
-                                Name = d.FirstName,
-                                LastName = d.LastName,
-                                Email = d.Email,
-                            }).ToList();
+	public class RecordsController : Controller
+	{
+		// GET: Records
+		public ActionResult Index(string search)
+		{
+			List<ListRecordsViewModels> list;
+			using (mydbEntities db = new mydbEntities())
+			{
+				var records = from d in db.Records
+							  select new ListRecordsViewModels
+							  {
+								  Id = d.Personid,
+								  Name = d.FirstName,
+								  LastName = d.LastName,
+								  Email = d.Email,
+							  };
 
-            }
-            return View(list);
-        }
+				if (!string.IsNullOrEmpty(Request.QueryString["search"]))
+				{
+					string search = Request.QueryString["search"];
+					records = records.Where(r => r.Name.Contains(search) || r.LastName.Contains(search));
+				}
 
-        public ActionResult New()
-        {
-            return View();
-        }
+				list = records.ToList();
+			}
 
-        [HttpPost]
-        public ActionResult New(RecordsViewModel model)
-        {
-            try
-            {
-                if(ModelState.IsValid)
-                {
-                    using( mydbEntities db = new mydbEntities())
-                    {
-                        var oElement = new Records();
-                        oElement.FirstName = model.Name;
-                        oElement.LastName = model.LastName;
-                        oElement.Email = model.Email;
-                        oElement.Date_of_birth = model.DateOfBirth;
+			ViewBag.Title = "Index";
 
-                        db.Records.Add(oElement);
-                        db.SaveChanges();
-                    }
-                    return Redirect("~/Records/");
-                }
-                return View(model);
-
-            }
-            catch (Exception ex) 
-            {
-                throw new Exception(ex.Message); 
-            }
-        }
-
-        public ActionResult Update(int id)
-        {
-            RecordsViewModel model = new RecordsViewModel();
-            using( mydbEntities db = new mydbEntities())
-            {
-                var oRecord = db.Records.Find(id);
-                model.Name = oRecord.FirstName;
-                model.LastName = oRecord.LastName;
-                model.Email = oRecord.Email;
-                model.DateOfBirth = (DateTime)oRecord.Date_of_birth;
-                model.Id = oRecord.Personid;
-            }
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult Update(RecordsViewModel model)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    using (mydbEntities db = new mydbEntities())
-                    {
-                        var oRecord = db.Records.Find(model.Id);
-                        oRecord.FirstName = model.Name;
-                        oRecord.LastName = model.LastName;
-                        oRecord.Email = model.Email;
-                        oRecord.Date_of_birth = model.DateOfBirth;
-
-                        db.Entry(oRecord).State = System.Data.Entity.EntityState.Modified;
-                        db.SaveChanges();
-                    }
-                    return Redirect("~/Records/");
-                }
-                return View(model);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public ActionResult Delete(int id)
-        {
-            using (mydbEntities db = new mydbEntities())
-            {
-                var oRecord = db.Records.Find(id);
-                db.Records.Remove(oRecord);
-                db.SaveChanges();
-            }
-            return Redirect("~/Records/");
-        }
+			return View(list);
+		}
 
 
-    }
+		public ActionResult New()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult New(RecordsViewModel model)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					using (mydbEntities db = new mydbEntities())
+					{
+						var oElement = new Records();
+						oElement.FirstName = model.Name;
+						oElement.LastName = model.LastName;
+						oElement.Email = model.Email;
+						oElement.Date_of_birth = model.DateOfBirth;
+
+						db.Records.Add(oElement);
+						db.SaveChanges();
+					}
+					return RedirectToAction("Index");
+				}
+				return View(model);
+
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public ActionResult Update(int id)
+		{
+			RecordsViewModel model = new RecordsViewModel();
+			using (mydbEntities db = new mydbEntities())
+			{
+				var oRecord = db.Records.Find(id);
+				model.Name = oRecord.FirstName;
+				model.LastName = oRecord.LastName;
+				model.Email = oRecord.Email;
+				model.DateOfBirth = (DateTime)oRecord.Date_of_birth;
+				model.Id = oRecord.Personid;
+			}
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult Update(RecordsViewModel model)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					using (mydbEntities db = new mydbEntities())
+					{
+						var oRecord = db.Records.Find(model.Id);
+						oRecord.FirstName = model.Name;
+						oRecord.LastName = model.LastName;
+						oRecord.Email = model.Email;
+						oRecord.Date_of_birth = model.DateOfBirth;
+
+						db.Entry(oRecord).State = System.Data.Entity.EntityState.Modified;
+						db.SaveChanges();
+					}
+					return RedirectToAction("Index");
+				}
+				return View(model);
+
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public ActionResult Delete(int id)
+		{
+			using (mydbEntities db = new mydbEntities())
+			{
+				var oRecord = db.Records.Find(id);
+				db.Records.Remove(oRecord);
+				db.SaveChanges();
+			}
+			return RedirectToAction("Index");
+		}
+	}
+
 }
